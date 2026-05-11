f5d3bc186ccec3c93dfd5c4890c32b750e64d3bd
Jedidiah Cheng <chengjedidiah@gmail.com>
2026-05-12 00:24:58 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{
+    io::{BufRead, BufReader, Read, Result, Write},
+    net::{TcpListener, TcpStream},
+};
+
+fn handle_connection(mut stream: TcpStream) -> Result<()> {
+    let reader = BufReader::new(stream.try_clone()?);
+
+    for line in reader.lines() {
+        let line = line?;
+        if line.starts_with("PING") {
+            stream.write_all(b"+PONG\r\n")?;
+        }
+    }
+
+    Ok(())
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,6 +29,7 @@ fn main() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                handle_connection(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
