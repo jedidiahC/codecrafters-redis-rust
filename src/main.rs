1c785d10acc5631e25c248e8b40d1b3811c79627
Jedidiah Cheng <chengjedidiah@gmail.com>
2026-05-13 23:33:09 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,40 @@
 #![allow(unused_imports)]
-use std::{
-    io::{BufRead, BufReader, Read, Result, Write},
-    net::{TcpListener, TcpStream},
-};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::net::{TcpListener, TcpStream};
 
-fn handle_connection(mut stream: TcpStream) -> Result<()> {
-    let reader = BufReader::new(stream.try_clone()?);
+async fn handle_connection(mut stream: TcpStream) -> anyhow::Result<()> {
+    let (reader, mut writer) = stream.split();
+    let mut buf_reader = BufReader::new(reader);
 
-    for line in reader.lines() {
-        let line = line?;
-        if line == "PING" {
-            stream.write_all(b"+PONG\r\n")?;
+    let mut line = String::new();
+
+    loop {
+        line.clear();
+
+        let bytes = buf_reader.read_line(&mut line).await?;
+        if bytes == 0 {
+            return Ok(());
+        }
+
+        if line.trim() == "PING" {
+            writer.write_all(b"+PONG\r\n").await?;
         }
     }
-
-    Ok(())
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment the code below to pass the first stage
     //
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-                handle_connection(_stream);
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+    loop {
+        let (stream, _) = listener.accept().await.unwrap();
+
+        println!("accepted new connection");
+        tokio::spawn(async move { handle_connection(stream).await });
     }
 }
