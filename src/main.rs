19f13c26ce7d18c7533f35c1a6be9264b76997d7
Jedidiah Cheng <chengjedidiah@gmail.com>
2026-06-06 17:01:06 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(unused_imports)]
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 use bytes::Bytes;
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
@@ -21,7 +22,17 @@ impl Resp for str {
     }
 }
 
-type Store = Arc<Mutex<HashMap<String, Bytes>>>;
+type Store = Arc<Mutex<HashMap<String, StoreValue>>>;
+
+/**
+ * We can store
+ * a value
+ * with various options such as EXP: UTC TIME
+ */
+struct StoreValue {
+    value: String,
+    options: HashMap<String, String>,
+}
 
 async fn read_command_arguments(reader: ReadHalf<'_>) -> Option<(String, Vec<String>)> {
     let mut buf_reader = BufReader::new(reader);
@@ -45,6 +56,7 @@ async fn read_command_arguments(reader: ReadHalf<'_>) -> Option<(String, Vec<Str
     let number_of_elements = first[1..].parse().ok()?;
     let mut args: Vec<String> = Vec::new();
 
+    // Read RESP array.
     for _ in 0..number_of_elements {
         line.clear();
 
@@ -71,7 +83,18 @@ async fn read_command_arguments(reader: ReadHalf<'_>) -> Option<(String, Vec<Str
 }
 
 // Process command, returning output string.
+// e.g. raw: SET mykey value PX 1000
+// command: SET
+// args: mykey, value, PX, 1000
+// args should be parsed to
+// key: mykey
+// value: value
+// option: {
+//   PX: 1000
+// }
 fn handle_command(command: &String, args: &Vec<String>, store: Store) -> Option<String> {
+    println!("COMMAND: {}, args: {}", command, args.join("|"));
+
     if command == "PING" {
         return Some("PONG".to_resp().to_string());
     }
@@ -82,10 +105,41 @@ fn handle_command(command: &String, args: &Vec<String>, store: Store) -> Option<
 
     if command == "SET" {
         let mut store = store.lock().unwrap();
-        let key = args[0].clone();
-        let value = Bytes::from(args[1..].join(" "));
 
-        store.insert(key, value);
+        let mut args_iter = args.iter().peekable();
+
+        // Parse args for keys and options
+        let key = args_iter.next()?.clone(); // 1st arg -> key
+        let value = args_iter.next()?.clone(); // 2nd arg -> value
+
+        // Parse 3rd arg onwards as options.
+
+        // Handle PX option.
+        // set utc time for expiry.
+        // We store a serialiable struct.
+
+        let mut options: HashMap<String, String> = HashMap::new();
+
+        while args_iter.peek() != None {
+            let arg = args_iter.next()?;
+            if arg == "EX" || arg == "PX" {
+                let exp_val_ms: u64 = args_iter.next()?.parse().unwrap();
+                let exp_time = match arg.as_str() {
+                    "EX" => (SystemTime::now() + Duration::from_secs(exp_val_ms))
+                        .duration_since(UNIX_EPOCH),
+                    _ => (SystemTime::now() + Duration::from_millis(exp_val_ms))
+                        .duration_since(UNIX_EPOCH),
+                };
+
+                let exp_opt_value = exp_time.unwrap().as_millis().to_string();
+
+                options.insert("EXP".to_string(), exp_opt_value);
+            }
+        }
+
+        let store_value = StoreValue { value, options };
+
+        store.insert(key, store_value);
         return Some("OK".to_resp().to_string());
     }
 
@@ -93,8 +147,28 @@ fn handle_command(command: &String, args: &Vec<String>, store: Store) -> Option<
         let store = store.lock().unwrap();
         let key = args[0].clone();
 
-        if let Some(value) = store.get(&key) {
-            return Some(String::from_utf8(value.to_vec()).ok()?.to_bulk_str());
+        if let Some(store_value) = store.get(&key) {
+            for (key, value) in &store_value.options {
+                println!("option: {}, {}", key, value);
+            }
+
+            // Check expiry if expired, delete value and return null bulk string $-1\r\n
+            if let Some(exp_time) = store_value.options.get(&("EXP".to_string())) {
+                let current_time = SystemTime::now()
+                    .duration_since(UNIX_EPOCH)
+                    .unwrap()
+                    .as_millis();
+                let exp_time: u128 = exp_time.parse().unwrap();
+                println!("current_time: {}, exp_time: {}", current_time, exp_time);
+
+                if exp_time < current_time {
+                    let null_bulk_string = String::from("$-1\r\n");
+                    return Some(null_bulk_string);
+                }
+            }
+
+            let value = store_value.value.clone();
+            return Some(value.to_bulk_str());
         } else {
             let null_bulk_string = String::from("$-1\r\n");
             return Some(null_bulk_string);
